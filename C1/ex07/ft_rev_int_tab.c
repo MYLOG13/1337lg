53de62dc5120c3c9b5903668d33ce7acbf24c437
Ismail Mellali <imellali@e1r11p8.1337.ma>
2024-06-27 15:07:18 +0100

    C01

---
+++ C1/ex07/ft_rev_int_tab.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 13:33:37 by imellali          #+#    #+#             */
+/*   Updated: 2024/06/27 13:46:00 by imellali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	start;
+	int	end;
+	int	temp;
+
+	start = 0;
+	end = size - 1;
+	temp = 0;
+	while (start < end)
+	{
+		temp = tab[start];
+		tab[start] = tab[end];
+		tab[end] = temp;
+		start++;
+		end--;
+	}
+}
