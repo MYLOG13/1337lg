0ab280576ec2d25ff6d411c459444ceba86ac1d2
Ismail Mellali <imellali@e1r13p5.1337.ma>
2024-07-12 20:50:14 +0100

    hi

---
+++ C8/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 08:55:19 by imellali          #+#    #+#             */
+/*   Updated: 2024/07/12 09:32:29 by imellali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ C8/ex01/ft_boolean.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 09:18:25 by imellali          #+#    #+#             */
+/*   Updated: 2024/07/12 10:22:35 by imellali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+#include <unistd.h>
+
+# define TRUE 1
+
+# define FALSE 0
+
+# define SUCCESS 0
+
+# define EVEN_MSG "I have an even number of arguments."
+
+# define ODD_MSG "I have an odd number of arguments."
+
+# define EVEN(number) (number % 2 == 0)
+
+typedef int t_bool;
+
+#endif
